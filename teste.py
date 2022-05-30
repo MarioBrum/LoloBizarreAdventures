cb4030309a1bc4661ecd1c519248759d8e3cc1cf
MarioBrum <42178811+MarioBrum@users.noreply.github.com>
2022-05-30 17:50:33 -0300

    Introdução aos arquivos do lolo e inicialização da janela

---
+++ teste.py
@@ -0,0 +1,25 @@
+import pygame
+
+import math
+import random
+
+import pygame
+from pygame import mixer
+
+# Intialize the pygame
+pygame.init()
+
+# create the screen
+screen = pygame.display.set_mode((800, 600))
+
+#Title and Icon
+pygame.display.set_caption("Lolo Bizarre Adventures")
+icon = pygame.image.load('loloIcon.png')
+pygame.display.set_icon(icon)
+
+#Game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
