9421330add13fa34b87a331e5e817adee7724d4a
MarioBrum <42178811+MarioBrum@users.noreply.github.com>
2022-06-20 20:28:50 -0300

    Criacao diagrama de classe e algumas alterações básicas nas classes de movimento

---
+++ JogoExecucao.py
@@ -0,0 +1,12 @@
+import pygame
+
+import math
+import random
+
+import pygame
+from pygame import mixer
+
+class Execucao:
+
+    def __init__(self,player):
+        self.player = player
--- playerLoloVISUAL.py
+++ playerLoloVISUAL.py
@@ -19,3 +19,13 @@ class loloVisual:
 
     def getY(self):
         return self.y
+
+    def setX(self,x):
+        self.x = x
+
+    def setY(self,y):
+        self.y = y
+    
+    def setXY(self,x,y):
+        self.x = x
+        self.y = y
