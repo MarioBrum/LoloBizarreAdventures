d0155af056c3fff4bcc26c78c9b3cc73eb9b55ea
MarioBrum <42178811+MarioBrum@users.noreply.github.com>
2023-01-15 00:23:29 -0300

    Mov. cursor no menu implementado

--- main.c
+++ main.c
@@ -1,22 +1,30 @@
 
+#include "stdio.h"
 #include "raylib.h"
-
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
+int menuOpcoes = 0;
+int maxOpcoes = 3;
+int cursorXbase = 177;
+int cursorYbase = 239;
+int cursorX = 177;
+int cursorY = 239;
 int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
     const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenHeight = 600;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-    Texture2D menu = LoadTexture("LoloMenu.png");
+    InitWindow(screenWidth, screenHeight, "Lolo Bizarre Adventures");
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-
+    // NOTE: Textures MUST be loaded after Window initialization (OpenGL context is required)
+    Texture2D menuInicial = LoadTexture("LoloMenu.png");        // Texture loading
+    Texture2D cursor = LoadTexture("loloCursorNOBG.png");
+    //---------------------------------------------------------------------------------------
+    //x = 177
+    //y = 239
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
@@ -30,22 +38,53 @@ int main(void)
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
-            //menu.show()
 
-            DrawText("o", 190, 200, 20, DARKBLUE);
-            //6ImageDraw(menu,menu,190,150,BLACK)
-            //ImageDrawRectangle(loadImage("LoloMenu.png"), 250, 200, 20, 20, DARKRED)
+            DrawTexture(menuInicial, screenWidth/2 - menuInicial.width/2, screenHeight/2 - menuInicial.height/2, WHITE);
+            DrawTexture(cursor, cursorX, cursorY, WHITE);
+            DrawText(TextFormat("Cursor X: %d",cursorX), 10, 10,30, WHITE);
+            DrawText(TextFormat("Cursor Y: %d",cursorY), 10, 60,30, WHITE);
+            if(IsKeyReleased(KEY_W)){
+                moveCursor(true);
+            }
+            if(IsKeyReleased(KEY_S)){
+                moveCursor(false);
+            }
 
 
         EndDrawing();
-        printf("%d",GetKeyPressed());
         //----------------------------------------------------------------------------------
     }
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
+    UnloadTexture(menuInicial);       // Texture unloading
+    UnloadTexture(cursor);       // Texture unloading
+
+    CloseWindow();                // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
 
     return 0;
+}
+
+//recebe um boolean, se for true o cursor sobe, caso contrario
+//o cursor desce
+void moveCursor(bool num){
+    if(num){
+        if(menuOpcoes == 0){
+            menuOpcoes = maxOpcoes;
+        }
+        else{
+            menuOpcoes--;
+        }
+    }
+    else{
+        if(menuOpcoes == maxOpcoes){
+            menuOpcoes = 0;
+        }
+        else{
+            menuOpcoes++;
+        }
+    }
+    cursorY = cursorYbase + 90*menuOpcoes;
+  
 }
